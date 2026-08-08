9c84538e33381500caac27d80c06f1428e808bff
SnrMario <dazzler.dfreaks@gmail.com>
2026-08-08 12:18:28 +0545

    Test

---
+++ practice.py
@@ -0,0 +1,8 @@
+#input function 
+full_name= input("Enter your full name: " )
+mobile_number= input("enter your mobile number: ")
+address=input("enter your address: ")
+age=input("enter your age : ")
+print( "The above Mentioned full name is :" , full_name , "and his mobile number is :" ,mobile_number , " and his age is : ", age ,"year")
+
+
